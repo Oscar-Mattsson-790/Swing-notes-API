719ae77c7ef326e248d68c445074bfe71a3a511c
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-04-25 09:26:42 +0200

    Ongoing

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,29 +2,44 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const User = require("../model/user");
 const crypto = require("crypto");
-const secret = crypto.randomBytes(64).toString("hex");
+
+const secret =
+  process.env.ACCESS_TOKEN_SECRET || crypto.randomBytes(64).toString("hex");
 process.env.ACCESS_TOKEN_SECRET = secret;
 
 console.log(secret);
 
 async function authToken(request, response, next) {
-  const authHeader = request.headers["authorization"];
-  const token = authHeader && authHeader.replace("Bearer ", "");
-
   try {
-    const payload = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    console.log("authToken middleware called");
+    console.log("headers:", request.headers);
+    const authHeader = request.headers["authorization"];
+    console.log("authHeader:", authHeader);
+    const token = authHeader && authHeader.split(" ")[1];
+    console.log("token:", token);
+
+    if (!token) {
+      console.log("Access token is missing");
+      return response.status(401).json({ message: "Access token is missing" });
+    }
+
+    const payload = await jwt.verify(token, secret);
     const user = await User.findOne({ uuid: payload.userId });
 
     if (!user) {
-      throw new Error();
+      console.log("User associated with the token not found");
+      return response
+        .status(401)
+        .json({ message: "User associated with the token not found" });
     }
 
     request.user = user;
-    request.token = token;
-    request.id = payload.userId;
     next();
-  } catch (error) {
-    response.json({ success: false, error: "Invalid token" });
+  } catch (err) {
+    console.error(err);
+    return response
+      .status(401)
+      .json({ message: "Invalid access token or secret" });
   }
 }
 
